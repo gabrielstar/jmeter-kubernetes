36755e6d82005a60115711dfe6c2a3a16f797a79
gabrielstar <gabriel.starczewski@gmail.com>
2020-02-21 09:57:30 +0100

    minifying pipeline to smallest working one

---
+++ start_test_from_script.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+#Script created to launch Jmeter tests directly from the current terminal without accessing the jmeter master pod.
+#It requires that you supply the path to the jmx file
+#After execution, test script jmx file may be deleted from the pod itself but not locally.
+
+working_dir="`pwd`"
+
+#Get namesapce variable
+tenant=`awk '{print $NF}' "$working_dir/tenant_export"`
+jmx="cloudssky.jmx"
+
+
+test_name="$(basename "$jmx")"
+
+#Get Master pod details
+
+master_pod=`kubectl get po -n $tenant | grep jmeter-master | awk '{print $1}'`
+
+kubectl cp "$jmx" -n $tenant "$master_pod:/$test_name"
+
+## Echo Starting Jmeter load test
+
+kubectl exec -ti -n $tenant $master_pod -- /bin/bash /load_test "$test_name"
